0c7f387cbbe79b426800141466af0114f7eea4ba
Mark kevin cachuela <mark.cachuela@neoday.com>
2024-07-08 23:40:30 +0800

    fix horizontal pager

--- app/src/main/java/com/example/travelbookingapp/features/home/HomeScreen.kt
+++ app/src/main/java/com/example/travelbookingapp/features/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.example.travelbookingapp.features.home
 
 import android.widget.Space
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,22 +16,58 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.travelbookingapp.R
 import com.example.travelbookingapp.components.images.RoundedImage
 import com.example.travelbookingapp.features.useraccount.builder.PersonBuilder
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+
+
+    val list = listOf(
+        R.drawable.icon_home,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_foreground
+    )
+//    val pagerState = rememberPagerState(pageCount = {
+//        7
+//    })
+    val pagerState = rememberPagerState(initialPage = 0) { maxOf(1, list.size) }
+    val currentIndex = remember { derivedStateOf { pagerState.currentPage } }
+    val coroutineScope = rememberCoroutineScope()
 
     val test = PersonBuilder.Builder()
         .setFirstname("Mark kevin")
@@ -51,6 +91,7 @@ fun HomeScreen(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
+                .padding(5.dp)
         ) {
             RoundedImage(
                 image = painterResource(R.drawable.login_background),
@@ -58,7 +99,7 @@ fun HomeScreen(
             )
             Text(text = "World")
 
-            Text(text = "tst")
+            Text(text = "test")
 
         }
 
@@ -72,57 +113,75 @@ fun HomeScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(20.dp)
+                    .clip(RoundedCornerShape(10.dp))
                     .background(Color.DarkGray)
             ) {
-
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Text(text = "Name place")
-                    Text(text = "sadasdas")
+                    Text(text = "What to do")
                 }
-                
+
                 Spacer(modifier = Modifier.height(250.dp))
 
             }
         }
 
+
+        val scroll = rememberScrollState()
+
         Box(
             modifier = Modifier
                 .padding(10.dp)
                 .background(Color.Green)
-                .fillMaxSize(),
-            contentAlignment = Alignment.BottomStart
+                .fillMaxSize()
+                .verticalScroll(scroll)
+
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
             ) {
 
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "asdadasd")
-                    Text(text = "asdadasd")
-                }
-
                 Spacer(modifier = Modifier.height(10.dp))
 
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth(),
+                        .fillMaxWidth()
+                        .padding(5.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(text = "asdadasd")
-                    Text(text = "asdadasd")
+                    Text(text = "Categories")
+                    Text(text = "See all")
                 }
 
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier.fillMaxSize(),
+                    pageSize = PageSize.Fixed(100.dp)
+                ) { page ->
+                    Column(
+
+                    ) {
+                        Image(
+                            modifier = Modifier.size(100.dp),
+                            painter = painterResource(id = list[page]),
+                            contentDescription = "image"
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .align(Alignment.CenterHorizontally),
+                            text = "KEVS"
+                        )
+                    }
+                }
             }
         }
     }
